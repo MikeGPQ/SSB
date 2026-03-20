7993e8241ab27f4ea03a6641986cdb58e07ab202
MikeGPQ <miguelgarciaparra@hotmail.com>
2026-03-20 12:43:45 -0600

    Eliminar mensaje de Log

--- FRONT-END/src/config/firebase.js
+++ FRONT-END/src/config/firebase.js
@@ -1,8 +1,6 @@
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
-console.log("Test API KEY:", import.meta.env.VITE_FIREBASE_API_KEY);
-
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
